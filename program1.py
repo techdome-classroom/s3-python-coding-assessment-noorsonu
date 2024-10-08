f4a099534a93bb3c2993d894939c4a6a7ef1622e
Noor Md <noorsonu11@gmail.com>
2024-10-08 15:16:41 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,14 +1,10 @@
-def isValid(s: str) -> bool:
-    # Stack to keep track of opening brackets
-    stack = []
-    
-    # Dictionary to map closing brackets to their corresponding opening brackets
-    bracket_map = {')': '(', '}': '{', ']': '['}
-    
-    # Iterate through each character in the string
-    for char in s:
-        # If it's a closing bracket
-        if
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        pass
 
 
 
